f6c0ff14fb933f7fd2a2869ad20d1d5ecf438bcc
Chrystian <163219742+VNXLab@users.noreply.github.com>
2024-03-13 07:51:24 +0100

    Update pdf_to_doc_docx.py

--- pdf_to_doc_docx.py
+++ pdf_to_doc_docx.py
@@ -1,7 +1,8 @@
-#Created by VNXLab
-#Website: https://vnxlab.uk/
-#Python guide: https://vnxlab.uk/how-to-run-python-on-windows-10-11/
-
+# Created by VNXLab
+# Website: https://vnxlab.uk/
+# Python guide: https://vnxlab.uk/how-to-run-python-on-windows-10-11/
+# Tested on Windows 10
+# =================================
 # pip install pdf2docx
 
 from pdf2docx import Converter
